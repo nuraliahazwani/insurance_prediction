3eef0a60070f7eaf17c711f9dacfda0df2b7e34e
nuraliahazwani <102709207+nuraliahazwani@users.noreply.github.com>
2022-04-04 16:52:41 +0800

    Update insurance_prediction.py

--- insurance_prediction.py
+++ insurance_prediction.py
@@ -4,11 +4,11 @@ import numpy as np
 
 def main():
 
-    st.title("Health Insurance Cost Prediction :hospital: :heavy_dollar_sign:")
+    st.title("Health Insurance Cost Prediction <font colour='blue'> :hospital: :heavy_dollar_sign:")
     
     model = joblib.load('model_joblib_gr')
     
-    st.subheader('This web app will predict the cost of health insurance according to certain classes. Data set used is a continuous dependent variable with 1,338 info. The model applied to this activity is Gradient Boosting Regression.')
+    st.text('This web app will predict the cost of health insurance according to certain classes. Data set used is a continuous dependent variable with 1,338 info. The model applied to this activity is Gradient Boosting Regression.')
     
     p1 = st.slider('Enter Your Age',18,100)
     
